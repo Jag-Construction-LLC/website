967f3a9c1f04d1db03a578617430818bc1af72d5
Anthony Marinov <anthonymmarinov@gmail.com>
2025-02-08 10:17:01 -0800

    feature: navbar tweaks

--- components/About.js
+++ components/About.js
@@ -48,12 +48,12 @@ export default function About() {
                     </div>
                     <div className="w-full mb-10 sm:mb-0 sm:w-1/2">
                         <div className="relative h-full ml-0 mr-0 sm:mr-10">
-                            <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-blue-700 rounded-lg"></span>
-                            <div className="relative h-full p-5 bg-white border-2 border-blue-700 rounded-lg">
+                            <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-blue-900 rounded-lg"></span>
+                            <div className="relative h-full p-5 bg-white border-2 border-blue-900 rounded-lg">
                                 <div className="flex items-center -mt-1">
                                     <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">Complex Project Management</h3>
                                 </div>
-                                <p className="mt-3 mb-1 text-xs font-medium text-blue-700 uppercase">------------</p>
+                                <p className="mt-3 mb-1 text-xs font-medium text-blue-900 uppercase">------------</p>
                                 <p className="mb-2 text-gray-600">Our success on large-scale, heavy-civil, and utility projects, as well as our experience with oil, gas, petroleum, and chemical plant upgrades, prove our capability to manage multifaceted construction endeavors.</p>
                             </div>
                         </div>
