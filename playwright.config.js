6bd4a48182ab6eddff86dded20dc5a3535971371
Anthony Marinov <anthonymmarinov@gmail.com>
2025-02-12 15:31:52 -0800

    feature: playwright test config updated

--- playwright.config.js
+++ playwright.config.js
@@ -1,18 +1,49 @@
+import { defineConfig, devices } from "@playwright/test";
 
 /** @type {import('playwright/test').PlaywrightTestConfig} */
-const config = {
+export default defineConfig({
     testDir: './tests',
+    fullyParallel: true,
+    reporter: 'html',
     timeout: 30 * 1000,
     use: {
-        headless: true,
         baseURL: 'http://localhost:3000',
-        viewport: { width: 1280, height: 720 },
         ignoreHTTPSErrors: true,
     },
-    reporter: [
-        ['list'],
-        ['html', { outputFolder: 'playwright-support', open: 'never'}]
-    ],
-};
-
-module.exports = config
+    projects: [
+        {
+            name: "chromium",
+            use: { ...devices["Desktop Chrome"] },
+        },
+    
+        {
+            name: "firefox",
+            use: { ...devices["Desktop Firefox"] },
+        },
+    
+        {
+            name: "webkit",
+            use: { ...devices["Desktop Safari"] },
+        },
+    
+        /* Test against mobile viewports. */
+        {
+            name: "Mobile Chrome",
+            use: { ...devices["Pixel 5"] },
+        },
+        {
+            name: "Mobile Safari",
+            use: { ...devices["iPhone 12"] },
+        },
+    
+        /* Test against branded browsers. */
+        {
+           name: 'Microsoft Edge',
+           use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        },
+        {
+            name: "Google Chrome",
+            use: { ...devices["Desktop Chrome"], channel: "chrome" },
+        },
+      ],
+});
